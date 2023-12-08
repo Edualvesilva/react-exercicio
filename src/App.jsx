0a558b1253416bfb129a147e3b972ba75a36b958
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-12-08 11:03:13 -0300

    Teste Com BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import Home from "./pages/Home";
 import Spiderman from "./pages/Spiderman";
 import Invincible from "./pages/Invincible";
 import Container from "./components/Container";
-
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <Container>
-        <Home />
-        <Spiderman />
-        <Invincible />
-      </Container>
+      <BrowserRouter>
+        <Container>
+          <Header />
+          <Routes>
+            <Route Component={Home} exact path="/" />
+          </Routes>
+        </Container>
+      </BrowserRouter>
     </>
   );
 }
